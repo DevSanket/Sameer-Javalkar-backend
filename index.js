767a47844e2b43ddfdc08793fc863ece5bf0599c
sanket <programmersanket@gmail.com>
2022-11-13 23:33:10 +0530

    Feture for Searching is Implemented

--- index.js
+++ index.js
@@ -41,9 +41,41 @@ app.get('/api/getAll',async (req,res) => {
 
 app.post('/api/search',async (req,res) => {
     const {search} = req.body;
-    await VotersDetails.find({"Voter Name" : {$regex : search,$options: 'i'}}).then((data) => {
+
+    const datas = String(search).toLowerCase().split(' ');
+
+    // await VotersDetails
+    // .find({"Voter Name" : {$regex : '^jav',$options: 'i'}}).then((data) => {
     //    res.json(data);
-       return res.status(200).json(data);
+    await VotersDetails.find({}).then((data) => {  
+    let newData= [];
+    data.map(user => {
+        // newData.push(user);
+        // return;
+        const splited = String(user["Voter Name"]).toLowerCase().split(' ');
+        // console.log(splited);
+        // splited.filter(item => item.startsWith('sam'|'jav')))
+        const checkArr = [];
+         datas.some(item => {
+            checkArr.push(splited.some(item2 => {
+               return item2.startsWith(item);
+            }));
+            
+        });
+
+        console.log(checkArr);
+
+        let value = checkArr.reduce((check1,check2) => check1 + check2,0);
+
+        if(value === datas.length){
+            newData.push(user);
+        }
+        
+
+        // String(user["Voter Name"]).search(`/${search}/i`)
+        
+    });
+       return res.status(200).json(newData);
     }).catch(err => {
         console.log(err);
         return res.status(400).json({msg:"No Data found"});
